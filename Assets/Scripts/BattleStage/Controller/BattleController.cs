d8b08e9cd1fe3ed282160ee52c170122b5daefe2
Le Minh Thanh <thanh-minh.le@vn.devinition.com>
2018-09-13 13:41:34 +0700

    Update code for spawn position

--- Assets/Scripts/BattleStage/Controller/BattleController.cs
+++ Assets/Scripts/BattleStage/Controller/BattleController.cs
@@ -5,9 +5,13 @@ namespace BattleStage.Controller
 {
     public class BattleController : MonoBehaviour
     {
+        
         private const string ENEMY_RESOURCE_FOLDER = "Prefabs/Enemies/";
         
         private const string ENEMY_RESOURCE_PREFIX = "Enemy_{0:D4}";
+
+        [SerializeField]
+        private Transform[] _positionsSpawn;
         
         void Start()
         {
@@ -21,7 +25,7 @@ namespace BattleStage.Controller
                 var path = string.Format(ENEMY_RESOURCE_FOLDER + ENEMY_RESOURCE_PREFIX,x.EnemyResourceId);
                 var enemyPrefab = Resources.Load(path);
 
-                Instantiate(enemyPrefab,x.Position,Quaternion.identity);
+                Instantiate(enemyPrefab,_positionsSpawn[Random.Range(0,_positionsSpawn.Length)].position,Quaternion.identity);
             });
         }
     }
--- Assets/Scripts/BattleStage/Domain/BaseUnitStatus.cs
+++ Assets/Scripts/BattleStage/Domain/BaseUnitStatus.cs
@@ -2,27 +2,81 @@
 {
     public class BaseUnitStatus
     {
-        public int HP { get; private set; }
-        
-        public int Attack { get; private set; }
-        
-        public int Speed { get; private set; }
-        
-       
-        private int _baseHP;
-        private int _baseAttack;
-        private int _baseSpeed;
+        public float HP
+        {
+            get { return _baseHP + (_levelHP * _percentHPByLevel * _baseHP); }
+        }
 
-        private int levelHP;
-        private int levelAttack;
-        private int levelSpeed;
+        public float Attack
+        {
+            get { return _baseAttack + (_levelAttack * _percentAttackByLevel * _baseAttack); }
+        }
         
-        private float percentHPByLevel;
-        private float percentAttackByLevel;
-        private float percentSpeedByLevel;
+        public float Speed
+        {
+            get { return _baseSpeed + (_levelSpeed * _percentSpeedByLevel * _baseSpeed); }
+        }
+       
+        /// <summary>
+        /// Base status for each character will diff not the same.
+        /// </summary>
+        private readonly int _baseHP;
+        private readonly int _baseAttack;
+        private readonly int _baseSpeed;
+
+        /// <summary>
+        /// Level (This will using Point we get for 2 wave to increase it)
+        /// </summary>
+        private int _levelHP;
+        private int _levelAttack;
+        private int _levelSpeed;
         
+        /// <summary>
+        /// Percent increase by level
+        /// </summary>
+        private readonly float _percentHPByLevel;
+        private readonly float _percentAttackByLevel;
+        private readonly float _percentSpeedByLevel;
+
+        /// <summary>
+        /// Constructure
+        /// </summary>
+        /// <param name="baseHp"></param>
+        /// <param name="baseAttack"></param>
+        /// <param name="baseSpeed"></param>
+        /// <param name="levelHp"></param>
+        /// <param name="levelAttack"></param>
+        /// <param name="levelSpeed"></param>
+        /// <param name="percentHpByLevel"></param>
+        /// <param name="percentAttackByLevel"></param>
+        /// <param name="percentSpeedByLevel"></param>
+        public BaseUnitStatus(int baseHp, int baseAttack, int baseSpeed, int levelHp, int levelAttack, int levelSpeed,
+            float percentHpByLevel, float percentAttackByLevel, float percentSpeedByLevel)
+        {
+            _baseHP = baseHp;
+            _baseAttack = baseAttack;
+            _baseSpeed = baseSpeed;
+            _levelHP = levelHp;
+            _levelAttack = levelAttack;
+            _levelSpeed = levelSpeed;
+            _percentHPByLevel = percentHpByLevel;
+            _percentAttackByLevel = percentAttackByLevel;
+            _percentSpeedByLevel = percentSpeedByLevel;
+        }
+
+        public void IncreaseHPLevel()
+        {
+            _levelHP++;
+        }
         
+        public void IncreaseAttackLevel()
+        {
+            _levelAttack++;
+        }
         
-    
+        public void IncreaseSpeedLevel()
+        {
+            _levelSpeed++;
+        }
     }
 }
